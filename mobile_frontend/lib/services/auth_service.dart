1564b8703fc76dcee610b70e9e90a74dc2dcafad
shuhan1272 <shuhan1272@gmail.com>
2026-09-06 22:16:12 +0600

    start command changed

--- mobile_frontend/lib/services/auth_service.dart
+++ mobile_frontend/lib/services/auth_service.dart
@@ -5,7 +5,7 @@ class AuthService {
   // ⚠️ CHANGE THIS TO YOUR BACKEND URL
   // If using Android Emulator: http://10.0.2.2:8000/api/
   // If using Chrome (web): http://127.0.0.1:8000/api/
-  static const String baseUrl = "http://127.0.0.1:8000/api/v1/accounts/";
+  static const String baseUrl = "http://10.0.2.2:8000/api/v1/accounts/";
 
   // =========================================================
   // REGISTER
